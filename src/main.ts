424c24b70b7449a3cb1c3bdf0a5483bfd8e84f80
Shane Bammel <sjb933@gmail.com>
2016-12-03 18:55:00 -0600

    - Cleanup of main - Testing commit from windows

--- src/main.ts
+++ src/main.ts
@@ -15,14 +15,6 @@ if (Config.USE_PATHFINDER) {
 
 log.info("load");
 
-/**
- * Screeps system expects this "loop" method in main.js to run the
- * application. If we have this line, we can be sure that the globals are
- * bootstrapped properly and the game loop is executed.
- * http://support.screeps.com/hc/en-us/articles/204825672-New-main-loop-architecture
- *
- * @export
- */
 export function loop() {
   // Check memory for null or out of bounds custom objects
   if (!Memory.uuid || Memory.uuid > 100) {
